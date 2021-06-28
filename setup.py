2af5370a2bc32741d0afdabc1fcb780bac2668c4
tybruno <tybruno@cisco.com>
2021-06-28 11:08:36 -0700

    made a small change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="Argparse Helper",
+    name="argparse_helper",
     version=".1",
     author="Tyler Bruno",
     packages=setuptools.find_packages(),
